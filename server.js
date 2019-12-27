30b4400dbef7ced8e594df16cc28ee2a997e9d32
thilakchelliah <thilak.chelliah49@gmail.com>
2019-12-27 06:47:41 +0000

    added:

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var fallback = require('express-history-api-fallback')
-var express = require('express') ;
+var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -15,9 +15,9 @@ var jwt = require('jsonwebtoken');
 //var localport = process.env.PORT != undefined ? process.env.PORT : 4500;
 
 
-var localport = process.env.PORT || 4200
-var localIp = process.env.IP ||"0.0.0.0"
-// '0.0.0.0'
+var localport = process.env.PORT || 4500;
+
+var localIp = process.env.IP || "localhost";
 console.log(localIp);
 console.log(localport);
 
@@ -41,7 +41,7 @@ global.multerUpload = multer({ storage: storage });
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-mongoose.connect("mongodb://thilaktest:test123@ds143070.mlab.com:43070/tech_registry_db",{ useNewUrlParser: true }, function (err, db) {
+mongoose.connect("mongodb://thilaktest:test123@ds143070.mlab.com:43070/tech_registry_db", { useNewUrlParser: true }, function (err, db) {
   if (err) {
     return console.dir(err);
   }
@@ -66,30 +66,22 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 //var root = __dirname + '/public'
 //app.use(express.static(root))
 //app.use(fallback('index.html', { root: root }))
 
 
+app.use('/api', apiRouteOpen);
+app.use('/apiS', middleWare, apiRouteSecured);
 
-
-app.get('*', function(req, res, next) {
-  if (req.url === '/' || req.url === '/admin'){
-	  res.sendFile(path.join(__dirname, 'public/index.html'));
-  }
-	else if(req.url === "/api"){
-		app.use('/api', apiRouteOpen);
-	}
-	else if(req.url === "/apiS"){
-		app.use('/apiS', middleWare, apiRouteSecured);
-}
- 
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 
@@ -103,15 +95,13 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  
 
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+  res.status(err.status).send(err)
 });
 app.listen(localport);
 
